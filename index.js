afaa24b1e5a40cb870f7ecdb60b089a12d7e9a21
Kamil Chowaniec <kamilchowaniec97@gmail.com>
2019-02-03 15:07:25 +0100

    Rooms are working, added console logs on Connections

--- index.js
+++ index.js
@@ -1,16 +1,102 @@
-const app = require('express')();
+var roomcount = 0;
 
-const server = require('http').createServer(app);
+class Game {
+  constructor(p1, p2) {
+    this.p1 = p1;
+    this.p2 = p2;
+    this.chess = new Chess();
+    this.room = "" + roomcount;
+    io.to(this.p1.id).emit("start",{
+        color:"w",
+        fen:this.chess.fen(),
+    });
+    io.to(this.p2.id).emit("start",{
+        color:"b",
+        fen:this.chess.fen(),
+    });
 
-const io = require('socket.io')(server);
 
-io.on('connection', () => {
 
+    this.p1.join(this.room);
+    this.p2.join(this.room);
+    let self = this;
+
+    this.getBoard = () => {
+      let letters = "abcdefgh";
+      let board = [];
+      for (let i = 0; i < 8; i++) {
+        board.push([]);
+        for (let j = 0; j < 8; j++) {
+          board[i].push(self.chess.get(letters[i] + (j + 1)));
+        }
+      }
+      return board;
+    };
+
+    this.move = (data)=> {
+        let move = self.chess.move(data);
+        if (move) {
+            console.log(move)
+          io.to(self.room).emit("move", move);
+        }
+      }
+
+    io.to(self.room).emit("board", self.getBoard());
+    self.p1.on("move", self.move);
+    self.p2.on("move", self.move);
+  }
+
+  
+}
+
+let games = [];
+
+const express = require("express");
+
+const app = express();
+
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+const Chess = require("chess.js").Chess;
+
+let waiting = null;
+
+io.on("connection", socket => {
+  console.log("New connection:", socket.id)
+  if (waiting) {
+    new Game(waiting, socket);
+    waiting = null;
+    roomcount++;
+    console.log("Created new Game room:",roomcount-1)
+  } else {
+    waiting = socket;
+    let s = waiting;
+    let disc = ()=>{
+      if(waiting && waiting.id == s.id) waiting = null;
+      console.log("Disconnected:",s.id);
+    }
+    waiting.on("disconnect", disc);
+  }
 });
 
-app.get("/",(req,res)=>{
-    res.send("HELLO WORLD   ");
-})
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-server.listen(process.env.PORT || 3000);
+app.get("/", (req, res) => {
+    let c = new Chess();
 
+  res.send(c.fen());
+  console.log("yesyesyesyes");
+});
+
+server.listen(process.env.PORT || 3001);
+
+app.use(express.static("public"));
