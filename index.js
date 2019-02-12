f47f965d302e0fb018d53c2d407ad7a523d38f25
Bartlomiej1997 <bartlomiejblaszczak1997@gmail.com>
2019-02-12 13:34:24 +0100

    fetch

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ colors.setTheme({
   action:'blue'
 })
 const express = require("express");
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 const app = express();
 
 
@@ -59,7 +59,7 @@ app.get("/rooms",(req,res)=>{
 })
 
 const PORT =process.env.PORT || 3001; 
-
+  
 server.listen(PORT, ()=>{
   console.log("Server has started on port:".info,`${PORT}`.id)
 });
